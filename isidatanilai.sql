50bbf15bf53b5bc03e9aa865bc3d26cf8b864df2
unknown <ferdiarka24@gmail.com>
2025-10-03 21:37:08 +0700

    menambahkan komentar pada isi data nilai

--- isidatanilai.sql
+++ isidatanilai.sql
@@ -1,7 +1,8 @@
-INSERT INTO nilai (siswa_id, mata_pelajaran, nilai)
+INSERT INTO nilai (siswa_id, mata_pelajaran, nilai) -- menambahkan data nilai siswa
 VALUES
 (1, 'Matematika', 85),
 (2, 'Bahasa Inggris', 90),
 (3, 'Fisika', 88),
 (4, 'Kimia', 92),
 (5, 'Ekonomi', 87);
+
