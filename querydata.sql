9c88cebf8373a3dbdd59c852254a57cfbb9ac6e0
unknown <ferdiarka24@gmail.com>
2025-10-03 21:41:59 +0700

    menambahkan komentar pada query data

--- querydata.sql
+++ querydata.sql
@@ -2,7 +2,8 @@ SELECT * FROM siswa;
 SELECT nama 
 FROM siswa
 WHERE jurusan = 'IPA';
-SELECT s.nama, AVG(n.nilai) AS rata_nilai
+SELECT s.nama, AVG(n.nilai) AS rata_nilai --rata rata nilai siswa
 FROM siswa s
 JOIN nilai n ON s.id = n.siswa_id
 GROUP BY s.nama;
+
--- update&delete.sql
+++ update&delete.sql
@@ -3,3 +3,4 @@ SET jurusan = 'IPA'
 WHERE nama = 'Ferdi';
 DELETE FROM nilai
 WHERE siswa_id = 1 AND mata_pelajaran = 'Matematika';
+
